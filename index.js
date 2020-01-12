f4b9e23964fe240bb53ffffc8acf8c9f97601c96
Oleg Kadyrov <abathojalyk@gmail.com>
2020-01-11 21:20:35 -0600

    Unit-testing with Mocha & Chai

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 function mult(a, b){
-    return a * b;
+  return a * b;
 }
 function isEvenNumber(number){
 
-    return number % 2 === 0;
+  return number % 2 === 0;
 }
 function isAdult(age){
-    if(age > 20){
-        return true
-    } else if(age >= 0 && age < 20){
-        return false
-    } else{
-        return undefined;
-    }
+  if(age > 20){
+    return true;
+  } else if(age >= 0 && age < 20){
+    return false;
+  } else{
+    return undefined;
+  }
 }
 function isPalindrome(str) {
-    if(typeof str !== "string")
-        return 'not string';
-    strReverse = str.split('').reverse().join('');
-    if (strReverse == str) {
-        return true;
-    }
+  if(typeof str !== 'string')
+    return 'not string';
+  strReverse = str.split('').reverse().join('');
+  if (strReverse == str) {
+    return true;
+  }
 }
 function sortArrayUp(array) {
-    array.sort(function (a, b) {
-        return (a - b);
-    });
-    return array
+  array.sort(function (a, b) {
+    return (a - b);
+  });
+  return array;
 }
 module.exports = {sum, mult, isEvenNumber, isAdult, isPalindrome, sortArrayUp};
